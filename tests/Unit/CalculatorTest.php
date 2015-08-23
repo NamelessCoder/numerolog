2005e0fac5f9ad2f6c8efb2979999da5807faad5
Claus Due <claus@namelesscoder.net>
2015-08-24 00:06:20 +0200

    [TASK] Add unit test skeletons and setup

---
+++ tests/Unit/CalculatorTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace NamelessCoder\Numerolog\Tests\Unit;
+
+/**
+ * Class CalculatorTest
+ */
+class CalculatorTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @test
+	 */
+	public function testMarkIncomplete() {
+		$this->markTestIncomplete('Not yet implemented');
+	}
+
+}
---
+++ tests/Unit/ServerTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace NamelessCoder\Numerolog\Tests\Unit;
+
+/**
+ * Class ServerTest
+ */
+class ServerTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @test
+	 */
+	public function testMarkIncomplete() {
+		$this->markTestIncomplete('Not yet implemented');
+	}
+
+}
